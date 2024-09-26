a2d579dbc0c1baec33b1d45a8b4ddca887228e63
Iqboljon <iqboljonummataliyev30@gmail.com>
2024-09-26 15:06:32 +0500

    updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 // Images
 import logo from "@/assets/footer/logo.svg";
-import greenTxt from "@/assets/footer/green-text.svg";
+import mainLogo from "@/assets/logo.svg";
+import twitter from "@/assets/footer/twitter.svg";
+import linkedin from "@/assets/footer/linkedin.svg";
+import facebook from "@/assets/footer/facebook.svg";
+import instagram from "@/assets/footer/instagram.svg";
+import { MdOutlineQuestionMark } from "react-icons/md";
 
 const Footer = () => {
   const linkStyle = "hover:underline cursor-pointer";
@@ -48,6 +53,27 @@ const Footer = () => {
             </ul>
           </div>
         </div>
+      </div>
+      <div className="links wrapper flex items-center gap-4">
+        <img src={twitter} className="w-7 h-7" alt="twitter icon" />
+        <img src={linkedin} className="w-7 h-7" alt="linkedin icon" />
+        <img src={facebook} className="w-7 h-7" alt="facebook icon" />
+        <img src={instagram} className="w-7 h-7" alt="instagram icon" />
+      </div>
+      <hr className="border-white my-10 max-w-7xl mx-auto" />
+      <div className="subFooter wrapper grid grid-cols-2 gap-y-4 lg:grid-cols-[2fr_3fr_4fr_4fr_5fr] place-items-center pb-10 text-slate-100">
+        <img src={mainLogo} className="w-12" alt="website logo" />
+        <div className="help flex items-center justify-center gap-2">
+          <div className="p-2 bg-slate-800 border-2 border-white flex items-center justify-center rounded-full">
+            <MdOutlineQuestionMark />
+          </div>
+          <p>Help center</p>
+        </div>
+        <p>Privacy Policy</p>
+        <p>Terms of Service</p>
+        <p className="col-span-2 lg:col-span-1">
+          All rights reserved by GameGeek | 2023
+        </p>
       </div>
     </footer>
   );
